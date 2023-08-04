dbcbe8aeb6df6f3d85104f1564793cc1bc2ffc77
Jarne <jarne.demeyer05@hotmail.com>
2023-08-04 14:02:14 +0200

    styling input and add task button

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -5,11 +5,14 @@ function TodoList() {
   return (
     <>
         <div className='content'>
-          <input type="text" id='input' />
-          <button id='add-task'>Add Task</button>
-          <ul className='task-list' id='tasks'>
-            
-          </ul>
+          <div className="top-content">
+            <input type="text" id='input' placeholder='Task...'/>
+            <button id='add-task'>Add Task</button>
+          </div>
+
+          <div className="bottom-content">
+            <ul className='task-list' id='tasks'></ul>
+          </div>
         </div>
     </>
   )
