5ae05f18e75bfd520d3873a3a4b4bfd6086981a8
Jarne <jarne.demeyer05@hotmail.com>
2023-08-05 17:29:37 +0200

    importing css file in other components

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
+import './App.css'
 
 function AddTask() {
   return (
-    
       <div className="top-content">
           <input type="text" id='input' placeholder='Task...'/>
           <button id='add-task'>Add Task</button>
