987d2e027f782344571791da1c7ec21268233eab
Jarne <jarne.demeyer05@hotmail.com>
2023-08-21 13:26:25 +0200

    saving work + add input refresh after adding task

--- src/AddTask.jsx
+++ src/AddTask.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function AddTask() {
+  const [message, setMessage] = useState('')
+
+  const handleMessage = (e) => {
+    var newMessage = e.currentTarget.value
+    // console.log(newMessage)
+    setMessage(newMessage)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log('Message submitted')
+    setMessage("")
+  }
   return (
       <div className="top-content">
-          <input type="text" id='input' placeholder='Task...'/>
-          <button id='add-task'>Add Task</button>
+          <input type="text" onChange={handleMessage} id='input' placeholder='Task...' value={message}/>
+          <button type='submit' onClick={handleSubmit} id='add-task' >Add Task</button>
       </div>
     
   )
